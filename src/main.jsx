eb5ee8798ca572acbf828aae0c213780c0d84958
Emily36863 <emily36863@gmail.com>
2025-10-14 18:25:49 +0100

    extra import

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { HashRouter } from "react-router-dom";
 /* ✅ global styles: import once, here */
 import "./styles/reset.css";
 import "./App.css";
+import "./app-override.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
